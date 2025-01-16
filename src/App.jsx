749ad4cec5860904facf1f152db6e6a3839b9731
Srikanthsrish <srishbontha83@gmail.com>
2025-01-16 19:46:30 +0530

    changes2 in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import Landing from './Components/Landing/Landing'
- import './App.css'
-import {  Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Landing from './Components/Landing/Landing';
 import Login from './Pages/LoginRegister/Login';
 import StudentLogin from './Pages/LoginRegister/StudentLogin';
-import AdminClasses from './Components/Admin/AdminDetails/Adminclasses';
 import AdminLogin from './Pages/LoginRegister/AdminLogin';
 import AdminRegister from './Pages/LoginRegister/AdminRegister';
 import TeacherLogin from './Pages/LoginRegister/TeacherLogin';
 import AdminDashboard from './Components/Admin/AdminDashboard';
-import StudentTimetable from './Components/Student/StudentDetails/StudentTimetable';
+import DashboardStudent from './Components/Student/DashboardStudent';
 import StudentProfile from './Components/Student/StudentDetails/StudentProfile';
 import StudentAttendance from './Components/Student/StudentDetails/StudentAttendance';
 import StudentComplains from './Components/Student/StudentDetails/StudentComplains';
-import DashboardStudent from './Components/Student/DashboardStudent';
+import StudentTimetable from './Components/Student/StudentDetails/StudentTimetable';
 import TeacherDetails from './Components/Teacher/TeacherDashboard';
 import Classes from './Components/Teacher/TeacherDetils/Classes';
 import TeacherProfile from './Components/Teacher/TeacherDetils/TeacherProfile';
@@ -23,51 +22,53 @@ import Complains from './Components/Admin/AdminDetails/Complains';
 import Teachers from './Components/Admin/AdminDetails/Teachers';
 import Students from './Components/Admin/AdminDetails/Students';
 import Subjects from './Components/Admin/AdminDetails/Subjects';
+import AdminClasses from './Components/Admin/AdminDetails/Adminclasses';
 import AdminHome from './Components/Admin/AdminDetails/AdminHome';
 import TeachersHome from './Components/Teacher/TeacherDetils/teachershome';
 import StudentHome from './Components/Student/StudentDetails/StudentHome';
+import './App.css';
 
 function App() {
-  
-
   return (
     <>
-
-     
       <Routes>
+        {/* General Routes */}
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/studentLogin" element={<StudentLogin/>} />
-        <Route path="/adminLogin" element={< AdminLogin/>} />
-        <Route path="//adminregister" element={<AdminRegister />} />
-        <Route path="/teacherLogin" element={<TeacherLogin/>} />
-        <Route path="/adminDashboard" element={<AdminDashboard/>} />
-        <Route path="/studentDashboard" element={<DashboardStudent/>} />
-        <Route path="/student/profile" element={<StudentProfile/>} />
-        <Route path="/student/attendance" element={<StudentAttendance/>} />
-        <Route path="/student/complains" element={<StudentComplains/>} />
-        <Route path="/student/timetable" element={<StudentTimetable/>} />
-        <Route path="/teacher-dashboard" element={<TeacherDetails/>} />
-        <Route path="teacher/classes" element={< Classes/>} />
-        <Route path="teacher/profile" element={< TeacherProfile/>} />
-        <Route path="teacher/assignments" element={<Assignments />} />
-        <Route path="/admin/profile" element={< AdminProfile/>} />
-        <Route path="/logout" element={< Landing/>} />
+
+        {/* Student Routes */}
+        <Route path="/student/login" element={<StudentLogin />} />
+        <Route path="/student/dashboard" element={<DashboardStudent />} />
+        <Route path="/student/profile" element={<StudentProfile />} />
+        <Route path="/student/attendance" element={<StudentAttendance />} />
+        <Route path="/student/complains" element={<StudentComplains />} />
+        <Route path="/student/timetable" element={<StudentTimetable />} />
+        <Route path="/student/home" element={<StudentHome />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/register" element={<AdminRegister />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/profile" element={<AdminProfile />} />
         <Route path="/admin/notices" element={<Notices />} />
         <Route path="/admin/complains" element={<Complains />} />
         <Route path="/admin/teachers" element={<Teachers />} />
-        <Route path="/admin/students" element={< Students/>} />
-        <Route path="/admin/subjects" element={< Subjects/>} />
-        <Route path="/admin/classes" element={<AdminClasses/>} />
+        <Route path="/admin/students" element={<Students />} />
+        <Route path="/admin/subjects" element={<Subjects />} />
+        <Route path="/admin/classes" element={<AdminClasses />} />
         <Route path="/admin/home" element={<AdminHome />} />
-        <Route path="teacher/dashboard" element={<TeachersHome/>} />
-        <Route path="/student/home" element={<StudentHome/>} />
-        
-        
-        </Routes>
-    
+
+        {/* Teacher Routes */}
+        <Route path="/teacher/login" element={<TeacherLogin />} />
+        <Route path="/teacher/dashboard" element={<TeacherDetails />} />
+        <Route path="/teacher/home" element={<TeachersHome />} />
+        <Route path="/teacher/classes" element={<Classes />} />
+        <Route path="/teacher/profile" element={<TeacherProfile />} />
+        <Route path="/teacher/assignments" element={<Assignments />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
+
